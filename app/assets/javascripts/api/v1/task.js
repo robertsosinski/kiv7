d8c906236fa4eb16521d03994720899137146715
Robert Sosinski <email@robertsosinski.com>
2012-03-22 23:07:05 -0400

    added unique extender

--- app/assets/javascripts/api/v1/task.js
+++ app/assets/javascripts/api/v1/task.js
@@ -1,17 +1,20 @@
 Api.V1.Task = function(data, opts) {
-  data = data || {};
-  opts = opts || {};
-  
   var self = this;
-  
+  var data = data || {};
+  var opts = opts || {};
+    
   self.id   = data.id;
   self.name = ko.observable(data.name).extend({
     presence: "Task name is required", 
     unique: {
-      scope: opts.scope, 
+      scope: opts.scope,
       errorMessage: "Task name must be unique"
     }
   });
   self.done = ko.observable(data.done);
   self.open = ko.computed(function() { return !self.done(); });
+  
+  self.name.hasError = ko.computed(function() {
+    return self.name.notPresent() || self.name.notUnique();
+  });
 };
