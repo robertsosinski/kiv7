4a6df6f1f88d28d0b67fadae1ae50b13fd8811ca
Robert Sosinski <email@robertsosinski.com>
2012-03-26 23:39:12 -0400

    various bug fixes

--- app/assets/javascripts/admin/stats_view_model.js
+++ app/assets/javascripts/admin/stats_view_model.js
@@ -2,13 +2,13 @@ Admin.StatsViewModel = function() {
   var self = this;
   
   self.stats = ko.observableArray([
-    {day: "Sunday", up: true},
-    {day: "Monday", up: true},
-    {day: "Tuesday", up: false},
-    {day: "Wednesday", up: true},
-    {day: "Thursday", up: false},
-    {day: "Friday", up: true},
-    {day: "Saturday", up: true},
+    {day: "Sunday",    up: ko.observable(true)},
+    {day: "Monday",    up: ko.observable(true)},
+    {day: "Tuesday",   up: ko.observable(false)},
+    {day: "Wednesday", up: ko.observable(true)},
+    {day: "Thursday",  up: ko.observable(false)},
+    {day: "Friday",    up: ko.observable(true)},
+    {day: "Saturday",  up: ko.observable(true)},
   ]);
 };
 
--- app/assets/javascripts/root/modal_view_model.js
+++ app/assets/javascripts/root/modal_view_model.js
@@ -5,6 +5,9 @@ Root.ModalViewModel = function() {
   self.list   = ko.observable();
   
   self.render = function(header, body) {
+    // format errors as a list, eg::
+    //  from: {"errors":{"name":["cant be blank", "must be unique"]}}
+    //    to: ["name cant be blank", "name must be unique"]
     var errors = _.reduce(body, function(memo, attr) {
       return _.map(_.keys(attr), function(field) {
         return field + " " + attr[field];
