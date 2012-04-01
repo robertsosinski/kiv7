afe4dc66193cd778ce2e2b2be11eba8ce92554d5
Robert Sosinski <email@robertsosinski.com>
2012-03-31 20:53:51 -0400

    more wip

--- config/routes/root.rb
+++ config/routes/root.rb
@@ -1,6 +1,8 @@
 Kiv7::Application.routes.draw do
   scope :module => :root do
-    scope '(:locale)', :locale => /en|fr|jp/ do
+    root :to => redirect('/en')
+    
+    scope ':locale', :locale => /en|fr|jp/ do
       root :to => 'pages#welcome'
     
       # routes all pages to an appropriate template
